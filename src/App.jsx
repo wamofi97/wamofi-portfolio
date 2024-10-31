2b1ad6753dbc52e9a2134558ccb318db5889380c
Wan Mohd Firdaus <wamofi97@gmail.com>
2024-10-31 21:03:36 +0800

    add contact

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import About from "./components/About";
+import Contact from "./components/Contact";
 import { Header } from "./components/Header";
 import Hero from "./components/Hero";
 import { Skills } from "./components/Skills";
@@ -7,14 +8,15 @@ import { Skills } from "./components/Skills";
 function App() {
   
   return (
-    <>
+    <main className="bg-neutral-900 text-neutral-200">
       <Header/>
       <main>
         <Hero/>
         <About/>
         <Skills/>
+        <Contact/>
       </main>
-    </>
+    </main>
   )
 }
 
---
+++ src/components/Contact.jsx
@@ -0,0 +1,17 @@
+export default function Contact() {
+  return (
+    <section id="skills" className="py-20 px-4">
+      <div className="max-w-6xl mx-auto px-4">
+        <div className="text-center mb-16">
+          <h2 className="text-4xl font-heading inline-flex items-center gap-2">
+            {/* <Heart className="w-8 h-8" /> */}
+            Contact
+          </h2>
+        </div>
+      </div>
+      <div className="grid grid-cols-2 mx-auto max-w-6xl">
+        
+      </div>
+    </section>
+  )
+}
