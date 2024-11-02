53b0386351c3d385c4f170150332409b4a0142cb
Wan Mohd Firdaus <wamofi97@gmail.com>
2024-11-03 04:24:43 +0800

    add footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <div className="flex items-center justify-center py-4 text-neutral-500">
+      <p>made with ❤️ by wamofi &copy; 2024</p>
+    </div>
+  )
+}
