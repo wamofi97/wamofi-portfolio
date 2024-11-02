36730e322b6c2af92ffb445109aeeba7e7624bce
Wan Mohd Firdaus <wamofi97@gmail.com>
2024-11-03 04:26:56 +0800

    add element for react-scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,38 @@
 import About from "./components/About";
 import Contact from "./components/Contact";
+import Experience from "./components/Experience";
+import Footer from "./components/Footer";
 import { Header } from "./components/Header";
 import Hero from "./components/Hero";
+import Project from "./components/Project";
 import { Skills } from "./components/Skills";
-
+import { Element } from "react-scroll";
 
 function App() {
   
   return (
-    <main className="bg-neutral-900 text-neutral-200">
+    <main className="bg-neutral-950 text-neutral-200">
       <Header/>
       <main>
-        <Hero/>
-        <About/>
-        <Skills/>
-        <Contact/>
+        <Element name="hero">
+          <Hero/>
+        </Element>
+        <Element name="skills">
+          <Skills/>
+        </Element>
+        <Element name="projects">
+          <Project/>
+        </Element>
+        <Element name="experiences">
+          <Experience/>
+        </Element>
+        <Element name="about">
+          <About/>
+        </Element>
+        <Element name="contact">
+          <Contact/>
+        </Element>
+        <Footer/>
       </main>
     </main>
   )
