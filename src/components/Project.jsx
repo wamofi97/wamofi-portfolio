f901e878a96210d37cbbaab211d19fd0ebb75a8b
Wan Mohd Firdaus <wamofi97@gmail.com>
2024-11-03 04:25:20 +0800

    add project page

---
+++ src/components/Project.jsx
@@ -0,0 +1,18 @@
+export default function Project() {
+    return (
+      <section id="project" className="min-h-[75vh] flex flex-col justify-center py-20 px-4">
+        <div className="max-w-6xl mx-auto px-4">
+          <div className="text-center mb-16">
+            <h3 className="text-teal-700 text-lg font-medium">Bringing ideas to life.</h3>
+            <h2 className="sm:text-6xl text-5xl font-heading inline-flex items-center gap-2">
+              PROJECTS
+            </h2>
+          </div>
+        </div>
+        <div className="grid grid-cols-2 mx-auto max-w-6xl">
+          
+        </div>
+      </section>
+    )
+  }
+  
