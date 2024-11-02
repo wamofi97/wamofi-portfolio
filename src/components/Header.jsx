203217110accd30f2a98c643c45d1ac1bc242f7f
Wan Mohd Firdaus <wamofi97@gmail.com>
2024-11-03 04:26:14 +0800

    add react-scroll/scrollspy

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,63 +1,69 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { Link,  scrollSpy  } from 'react-scroll';
 import { Github, Linkedin, Mail, Menu, X } from 'lucide-react';
 
 export function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  // const [activeSection, setActiveSection] = useState('');
+
+  useEffect(() => {
+    // Events.scrollEvent.register('begin', () => {});
+    // Events.scrollEvent.register('end', (to) => {
+    //   setActiveSection(to);
+    // });
+
+    scrollSpy.update();
+
+    // return () => {
+    //   Events.scrollEvent.remove('begin');
+    //   Events.scrollEvent.remove('end');
+    // };
+  }, []);
 
   return (
-    <header className={`fixed top-0 left-0 right-0 z-10 transition-all ease-in-out backdrop-blur-sm shadow-md ${isMenuOpen ? "h-screen" : "h-16"}`}>
+    <header className={`fixed top-0 left-0 right-0 z-10 transition-all ease-in-out bg-neutral-950/70 backdrop-blur-sm  shadow-xl `}>
       <nav className="max-w-5xl mx-auto px-4 py-4">
         <div className="flex items-center justify-between ">
-          <div className="text-2xl font-bold gradient-text">wamofi.dev</div>
+          <Link to="hero" duration={700} smooth={true} className="cursor-pointer text-2xl font-bold gradient-text">wamofi.dev</Link>
           
            {/* Mobile menu button */}
           <button 
             onClick={() => setIsMenuOpen(!isMenuOpen)}
-            className="md:hidden text-gray-600 hover:text-slate-600 transition-all z-10"
+            className="md:hidden text-neutral-600 hover:text-neutral-400 transition-all z-10"
           >
-            {isMenuOpen ? <X className="w-6 h-6" /> : <Menu className="w-6 h-6" />}
+            {isMenuOpen ? <X className="w-8 h-8" /> : <Menu className="w-8 h-8" />}
           </button>
 
           {/* Desktop menu */}  
           <div className="hidden md:flex items-center gap-8">
-            <div className="flex items-center gap-6">
-              <a href="#about" className="hover:text-teal-600 transition-colors text-lg">About</a>
-              <a href="#projects" className="hover:text-teal-600 transition-colors text-lg">Projects</a>
-              <a href="#skills" className="hover:text-teal-600 transition-colors text-lg">Skills</a>
-              <a href="#contact" className="hover:text-teal-600 transition-colors text-lg">Contact</a>
-            </div>
-            <div className="flex items-center gap-4 ">
-              <a href="https://github.com/wamofi97"  target='_blank'className="transition-colors">
-                <Github className="w-5 h-5" />
-              </a>
-              <a href="https://linkedin.com/in/wanfirdausfauzi" target='_blank' className="transition-colors">
-                <Linkedin className="w-5 h-5" />
-              </a>
-              <a href="mailto:wamofi97@gmail.com" target='_blank' className="transition-colors">
-                <Mail className="w-5 h-5" />
-              </a>
+            <div className="flex items-center gap-8">
+              <Link to="skills" spy={true} duration={700} smooth={true}  activeClass="text-teal-400 underline underline-offset-4 decoration-2" className="cursor-pointer hover:text-teal-400 transition-colors text-lg">Skills</Link>
+              <Link to="projects" spy={true} duration={700} smooth={true}  activeClass="text-teal-400 underline underline-offset-4 decoration-2 "  className="cursor-pointer hover:text-teal-400 transition-colors text-lg" >Projects</Link>
+              <Link to="experiences" spy={true} duration={700} smooth={true}  activeClass="text-teal-400 underline underline-offset-4 decoration-2 "  className="cursor-pointer hover:text-teal-400 transition-colors text-lg" >Experiences</Link>
+              <Link to="about" spy={true} duration={700} smooth={true}  activeClass="text-teal-400 underline underline-offset-4 decoration-2 "  className="cursor-pointer hover:text-teal-400 transition-colors text-lg">About</Link>
+              <Link to="contact" spy={true} duration={700} smooth={true} activeClass="text-teal-400 underline underline-offset-4 decoration-2 "  className="cursor-pointer hover:text-teal-400 transition-colors text-lg" >Contact</Link>
             </div>
           </div>
         </div>
 
         {/* Mobile menu */}    
         
-        <div className={`md:hidden transition-all duration-500 ease-in-out bg-slate-700 shadow-xl  min-h-screen w-3/4 sm:w-1/2 absolute top-0 right-0 ${isMenuOpen ? 'translate-x-0' : 'translate-x-full'} pt-20`}>
+        <div className={`md:hidden transition-all duration-500 ease-in-out bg-neutral-950/95   min-h-screen w-3/4 sm:w-1/2 absolute top-0 right-0 ${isMenuOpen ? 'translate-x-0' : 'translate-x-full'} pt-20`}>
           <div className="flex flex-col items-center gap-20">
-            <div className='flex flex-col items-center gap-4 py-4 '>
-                <a href="#about" className="text-slate-500 hover:scale-105 transition-all duration-200 ease-in-out text-2xl w-fit">About</a>
-                <a href="#projects" className="text-slate-500 hover:text-slate-700 transition-colors text-2xl w-fit">Projects</a>
-                <a href="#skills" className="text-slate-500 hover:text-slate-700 transition-colors text-2xl w-fit">Skills</a>
-                <a href="#contact" className="text-slate-500 hover:text-slate-700 transition-colors text-2xl w-fit">Contact</a>
+            <div className='flex flex-col items-center gap-8 py-4 text-2xl'>
+              <Link to="skills" smooth={true} className="cursor-pointer hover:text-teal-600 transition-colors ">Skills</Link>
+              <Link to="about" smooth={true} className="cursor-pointer hover:text-teal-600 transition-colors ">About</Link>
+              <Link to="projects" smooth={true} className="cursor-pointer hover:text-teal-600 transition-colors ">Projects</Link>
+              <Link to="contact" smooth={true} className="cursor-pointer hover:text-teal-600 transition-colors ">Contact</Link>
             </div>
             <div className="flex justify-center items-center gap-8">
-              <a href="https://github.com/wamofi97" target='_blank' className="text-slate-500 hover:text-slate-700 transition-colors">
+              <a href="https://github.com/wamofi97" target='_blank' className="text-neutral-400 hover:text-slate-700 transition-colors">
                 <Github className="text-3xl" />
               </a>
-              <a href="https://linkedin.com/in/wanfirdausfauzi" target='_blank' className="text-slate-500 hover:text-slate-700 transition-colors">
+              <a href="https://linkedin.com/in/wanfirdausfauzi" target='_blank' className="text-neutral-400 hover:text-slate-700 transition-colors">
                 <Linkedin className="text-3xl" />
               </a>
-              <a href="mailto:wamofi97@gmail.com" className="text-slate-500 hover:text-slate-700 transition-colors">
+              <a href="mailto:wamofi97@gmail.com" className="text-neutral-400 hover:text-slate-700 transition-colors">
                 <Mail className="text-3xl" />
               </a>
             </div>
