67d77f430941938f2c1b1d677d6edadb3b745ecf
Wan Mohd Firdaus <wamofi97@gmail.com>
2024-11-03 04:26:24 +0800

    add experience page

---
+++ src/components/Experience.jsx
@@ -0,0 +1,18 @@
+export default function Experience() {
+    return (
+      <section id="project" className="min-h-[75vh] flex flex-col justify-center py-20 px-4">
+        <div className="max-w-6xl mx-auto px-4">
+          <div className="text-center mb-16">
+            <h3 className="text-teal-700 text-lg font-medium">Work and Education.</h3>
+            <h2 className="sm:text-6xl text-4xl font-heading inline-flex items-center gap-2">
+              EXPERIENCES
+            </h2>
+          </div>
+        </div>
+        <div className="grid grid-cols-2 mx-auto max-w-6xl">
+          
+        </div>
+      </section>
+    )
+  }
+  
