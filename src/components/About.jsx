096a15cac7bed7a564e27fdb174d09032ff0d7b3
Wan Mohd Firdaus <wamofi97@gmail.com>
2024-11-05 22:03:07 +0800

    remove min-height and add dark/light color

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,27 +2,27 @@
 
 export default function About() {
   return (
-    <section id="about" className="min-h-[75vh] flex flex-col justify-center py-20 px-4">
+    <section id="about" className="flex flex-col justify-center pt-20 px-4">
       <div className="max-w-5xl mx-auto px-4">      
         <div className="text-center mb-16">
-          <h3 className="text-teal-700 text-lg font-medium">Get to know me.</h3>
           <h2 className="sm:text-6xl text-5xl font-heading font-bold inline-flex items-center gap-2">
             ABOUT
           </h2>
+          <h3 className="text-teal-700 text-lg font-medium">Get to know me.</h3>
         </div>
 
         
         <div>
-            <p className="text-neutral-400 text-lg leading-relaxed sm:text-center mb-8 font-light">
+            <p className="text-neutral-700 dark:text-neutral-400 text-lg leading-relaxed sm:text-center mb-8">
                 After graduating with a degree in{" "}
-                <span className="font-bold">Aerospace Engineering</span>, I decided to pursue my
+                <span className="font-medium">Aerospace Engineering</span>, I decided to pursue my
                 passion for programming. I enrolled in a coding bootcamp and learned{" "}
                 <span className="font-medium">full-stack web development</span>.{" "}
                 <span className="italic">My favorite part of programming</span> is the
                 problem-solving aspect. I <span className="underline">love</span> the
                 feeling of finally figuring out a solution to a problem. My core stack
                 is{" "}
-                <span className="font-medium">
+                <span className="font-bold">
                 React, Next.js, Node.js, and PostgreSQL
                 </span>
                 . I am also familiar with TypeScript and Drizzle. I am always looking to
@@ -31,7 +31,7 @@ export default function About() {
                 developer.
             </p>
 
-            <p className="text-neutral-400 text-lg leading-relaxed sm:text-center font-light">
+            <p className="text-neutral-700 dark:text-neutral-400 text-lg leading-relaxed sm:text-center">
                 <span className="italic">When I'm not coding</span>, I enjoy playing
                 video games and watching movies. I also enjoy{" "}
                 <span className="font-medium">learning new things</span>. I'm currently
