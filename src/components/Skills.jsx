8f83af40871b7fa6cf86c705edcf2cb379cdba1e
Wan Mohd Firdaus <wamofi97@gmail.com>
2024-11-05 22:05:50 +0800

    add blur spots in background

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,5 +1,4 @@
 import figma from "../assets/logos/figma.svg";
-import git from "../assets/logos/git.svg";
 import react from "../assets/logos/react.svg";
 import supabase from "../assets/logos/supabase.svg";
 import tailwind from "../assets/logos/tailwind.svg";
@@ -11,14 +10,24 @@ const skills = [{
     desc: "Design tool"
   },
   {
-    title: "Git",
-    source: git ,
+    title: "Github",
+    source: "https://github.com/favicon.ico",
     desc: "Version control"
   },
   {
     title: "React",
     source: react,
     desc: "Frontend framework"
+  },
+  {
+    title: "TypeScript",
+    source: typescript,
+    desc: "Language"
+  },
+  {
+    title: "Supabase",
+    source: supabase,
+    desc: "Database"  
   },
   {
     title: "Tailwind",
@@ -50,29 +59,32 @@ const skills = [{
 
 export function Skills() {
   return (
-    <section id="skills" className="min-h-[75vh] flex flex-col justify-center py-20 px-4">
+    <section id="skills" className="h-full flex flex-col justify-center py-20 px-4 relative">
+      
       <div className="max-w-5xl mx-auto px-4">
         <div className="text-center mb-16">
-          <h3 className="text-teal-700 text-lg font-medium">My go-to technologies.</h3>
           <h2 className="sm:text-6xl text-5xl font-heading inline-flex items-center gap-2">
             SKILLS
           </h2>
+          <h3 className="text-teal-700 text-lg font-medium">My go-to technologies.</h3>
           
         </div>
       </div>
-      <div className="grid grid-cols-2 md:grid-cols-4 gap-2 sm:gap-4 mx-auto max-w-5xl">
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-2 sm:gap-4 mx-auto max-w-5xl z-10">
         {skills.map((skill, index) => (
-            <div key={index} className="group bg-neutral-900/50 hover:bg-teal-900/20 rounded-lg p-6 hover:animate-float">
+            <div key={index} className="group bg-neutral-300/40 dark:bg-neutral-900/50 hover:bg-teal-600/10 dark:hover:bg-teal-900/20 rounded-2xl p-6 hover:animate-float">
                 <div className=" flex flex-col sm:flex-row items-center gap-2 sm:gap-4 ">
                     <img src={skill.source} alt={skill.title} className="w-8 h-8 group-hover:scale-125 group-hover:rotate-12 transition-transform duration-300" />
                     <div>
                         <h3 className="text-xl font-body">{skill.title}</h3>
-                        <p className="text-neutral-400 text-sm hidden sm:block">{skill.desc}</p>
+                        <p className="text-neutral-500 text-sm hidden sm:block">{skill.desc}</p>
                     </div>
                 </div>
             </div>
         ))}
       </div>
+      <div className="absolute top-10 right-0 sm:w-1/5 w-1/2 h-1/3 bg-teal-400 rounded-2xl blur-3xl opacity-20"></div>
+      <div className="absolute -bottom-32 left-10 sm:w-1/5 w-1/2 h-1/3 bg-teal-400 rounded-3xl blur-3xl opacity-20"></div>
     </section>
   );
 }
