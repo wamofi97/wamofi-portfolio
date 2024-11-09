f73c755044585e7190ac496bef625ff35e50f23f
Wan Mohd Firdaus <wamofi97@gmail.com>
2024-11-10 05:57:48 +0800

    edit position toggle button

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -25,12 +25,12 @@ const ThemeToggle = () => {
   return (
     <div className="fixed max-w-5xl z-30 top-0 w-full"> 
       <button onClick={toggleDarkMode} 
-      className={`absolute md:top-4 top-[74px] md:right-4 right-2 flex items-center justify-center bg-neutral-200 rounded-2xl w-11 h-11 transition-colors 
-      ${darkMode ? ' bg-neutral-800 hover:bg-neutral-700' : ' bg-neutral-300/80 hover:bg-neutral-400'} `}>
+      className={`absolute md:top-[23px] top-[11px] md:right-4 right-[72px] flex items-center justify-center bg-neutral-200 rounded-2xl w-10 h-10 transition-colors bg-transparent
+      ${darkMode ? 'bg-transparent md:bg-neutral-800 md:hover:bg-neutral-700' : 'bg-transparent md:bg-neutral-300/80 md:hover:bg-neutral-400'}`}>
       {darkMode ? (
-        <Moon className="w-6 h-6" />
+        <Moon className="w-7 h-7" />
       ) : (
-        <Sun className="w-6 h-6" />
+        <Sun className="w-7 h-7" />
       )}
 
       </button>
