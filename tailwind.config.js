1f457151965d4ceaf2f1e199c845f4e64ca1e9fc
Wan Mohd Firdaus <wamofi97@gmail.com>
2024-11-10 05:54:22 +0800

    edit screen size width

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,14 @@ export default {
         'logo': ['JetBrainsMono-Variable', 'monospace'],
         'heading': ['ClashDisplay-Medium', 'sans-serif'],
         'body': ['Satoshi-Variable', 'sans-serif'],
+      },
+      screens: {
+        'xs': '375px',
+        'sm': '425px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
       }
     },
   },
