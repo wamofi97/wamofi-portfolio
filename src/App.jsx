7f4e07f91475aa5506b37f454005abb47ef278f7
Wan Mohd Firdaus <wamofi97@gmail.com>
2024-11-10 05:54:39 +0800

    min h-screen sections

--- src/App.jsx
+++ src/App.jsx
@@ -7,36 +7,35 @@ import Hero from "./components/Hero";
 import Project from "./components/Project";
 import { Skills } from "./components/Skills";
 import { Element } from "react-scroll";
-import ThemeToggle from "./components/ThemeToggle";
 import DottedBackground from "./components/DottedBackground";
 
 function App() {
 
   return (
-    <div className="w-full relative   text-neutral-900 dark:text-neutral-200 transition-colors duration-500">
+    <div className="w-full relative text-neutral-900 dark:text-neutral-200 transition-colors duration-500 overflow-hidden ">
       <Header/>
       <main className="max-w-5xl mx-auto relative overflow-hidden md:overflow-visible " >
         <Element name="hero" className="min-h-screen flex justify-center items-center">
             <Hero/>
         </Element>
-        <Element name="skills" className=" ">
+        <Element name="skills" className="min-h-screen flex justify-center items-center">
           <Skills/>
         </Element>
-        <Element name="projects" className=" ">
+        <Element name="projects" className=" min-h-screen flex justify-center items-center">
             <Project/>
         </Element>
-        <Element name="experiences" className=" ">
+        <Element name="experiences" className=" min-h-screen flex justify-center items-center">
           <Experience/>
         </Element>
-        <Element name="about" className=" ">
+        <Element name="about" className="min-h-screen flex justify-center items-center">
               <About/>
         </Element>
-        <Element name="contact" className="min-h-screen">
+        <Element name="contact" className="min-h-screen flex justify-center items-center">
               <Contact/>
         </Element>
         <Footer/>
         <DottedBackground/>
-        <ThemeToggle/>
+
       </main>
     </div>
   )
