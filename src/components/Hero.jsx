207d51bdcba8aa24bb75c0ce819612644f6c45c6
Wan Mohd Firdaus <wamofi97@gmail.com>
2024-11-15 00:23:29 +0800

    update ui and copywriting for hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,48 +1,34 @@
-// import profile1 from '../assets/profile.webp';
-// import profile2 from '../assets/frame2.png';
-// import profile3 from '../assets/frame3.png';
-import profile4 from '../assets/frame4.png';
+import profile from '../assets/frame.png';
 import resume from '/resume.pdf';
 import { Link } from 'react-scroll'
-
-import { Github, Linkedin, Mail, FileUser,  ChevronRight } from 'lucide-react';
+import { Github, Linkedin, Mail, BookUser, ChevronRight } from 'lucide-react';
+import malaysia from '/malaysia.svg';
 
 export default function Hero() {
   
   return (
-    <section className="flex z-10 items-center justify-center pt-24 pb-20 lg:scale-[1.3] ">
-      
-      <div className="max-w-5xl px-4 py-20 flex flex-wrap gap-x-8 gap-y-8 items-center justify-center">
-        <div className='flex flex-col gap-4 items-center'>
-          <span className='text-3xl'><span className=" animate-wave">👋 </span> Hello</span>
-          <div className="relative group mx-auto">
-            <div className="absolute inset-0 bg-gradient-to-r from-teal-200 to-teal-400 rounded-full blur-xl opacity-30 group-hover:opacity-40 transition-opacity"></div>
-            <div className="relative rounded-full shadow-xl rotate-3 hover:rotate-0 transition-transform duration-300">
-              <img 
-                src={profile4}
-                alt="Profile"
-                className="h-40 w-40 object-cover rounded-full transition-all duration-300"
-              />
-            </div>
+    <section className="flex md:flex-row flex-col items-center justify-center gap-8 py-52 px-4">
+        <div className="relative group flex flex-col items-center justify-center">
+          <div className="absolute inset-0 bg-gradient-to-r from-teal-200 to-teal-400 rounded-full blur-2xl opacity-40 group-hover:opacity-70 transition-opacity"></div>
+          <div className="relative rounded-full shadow-xl rotate-3 hover:rotate-0 transition-transform duration-300">
+            <img 
+              src={profile}
+              alt="Profile"
+              className="h-52 min-w-52 object-cover rounded-full transition-all duration-300"
+            />
           </div>
-        </div>
-
-        <div className="space-y-2 text-center">
-          <div className="space-y-4 text-center">
-            
-            <h1 className="sm:text-5xl text-4xl font-body font-bold">
-              <span className='font-medium'>My name is </span>
-              <span className="text-neutral-800 dark:text-neutral-50">Wan Firdaus</span> 
-              <span className='text-lg font-light block'>and I'm a</span>
-              <span className="block font-heading tracking-wide gradient-text">FRONTEND DEVELOPER</span>
-            </h1>
-          </div>
-          <p className="text-lg font-light leading-relaxed ">
-            Bringing ideas to life with modern web technologies and clean design.
-          </p>
           
-          <div className="flex flex-wrap justify-center items-center gap-3">
-            
+        </div>
+        <div className="space-y-4">
+          <div className="space-y-2 flex flex-col md:text-start text-center">
+            <p className='z-10 w-fit sm:mx-0 mx-auto bg-neutral-300/50 dark:bg-neutral-800/50 px-4 py-2 rounded-full text-sm flex items-center'><span className='w-[6px] h-[6px] inline-block rounded-full bg-teal-600 dark:bg-teal-300 mr-2 animate-pulse'></span>Available for work</p>  
+            <h1 className='lg:text-6xl sm:text-5xl text-3xl font-bold'> Hi, I'm <span className='gradient-text'>Wan Firdaus</span> <span className=" animate-wave">👋 </span></h1>
+            <h2 className="md:text-3xl sm:text-2xl text-xl">A <span className=' font-bold'>frontend developer</span> that love to learn and build things. Based from Malaysia
+            <img className='w-8 ml-2 inline' src={malaysia} alt="malaysia flag" />
+            </h2>
+          </div>
+          
+          <div className="flex flex-wrap md:justify-start justify-center items-center gap-3">
             <div className="flex items-center justify-center gap-1">
               <a href="https://github.com/wamofi97" target='_blank'  className="relative group bg-neutral-300/80 dark:bg-neutral-800  p-3 rounded-2xl transition-all duration-300 hover:-translate-y-1">
                 <Github className="w-5 h-5" />
@@ -63,7 +49,7 @@ export default function Hero() {
                 </span>
               </a>
               <a href={resume} target='_blank'  className="flex items-center gap-1 border border-neutral-400/30 dark:border-neutral-700 bg-neutral-300/80 dark:bg-neutral-800 px-4 py-2 rounded-2xl transition-all duration-300 hover:-translate-y-1">
-                <FileUser className="w-5 h-5" />
+                <BookUser className="w-5 h-5"/>
                 Resume
               </a>
             </div>
@@ -82,8 +68,7 @@ export default function Hero() {
               </span>
             </Link>
           </div>
-        </div>     
-      </div>
+        </div>  
     </section>
   );
 }
