8983a879b9d3744a864229f604fbfffb0923687a
Wan Mohd Firdaus <wamofi97@gmail.com>
2024-11-15 03:34:23 +0800

    add base from malaysia

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -21,10 +21,10 @@ export default function Hero() {
         </div>
         <div className="space-y-4">
           <div className="space-y-2 flex flex-col md:text-start text-center">
-            <p className='z-10 w-fit md:mx-0 mx-auto bg-neutral-300/50 dark:bg-neutral-800/50 px-4 py-2 rounded-full text-sm flex items-center'><span className='w-[6px] h-[6px] inline-block rounded-full bg-teal-600 dark:bg-teal-300 mr-2 animate-pulse'></span>Available for work</p>  
-            <h1 className='lg:text-6xl md:text-5xl sm:text-4xl text-3xl font-bold'> Hi, I'm <span className='gradient-text'>Wan Firdaus</span> <span className=" animate-wave">👋 </span></h1>
-            <h2 className="md:text-3xl sm:text-2xl text-xl">A <span className=' font-bold'>frontend developer</span> that love to learn and build things. Based from Malaysia
-            <img className='w-8 ml-2 inline' src={malaysia} alt="malaysia flag" />
+            {/* <p className='z-10 w-fit md:mx-0 mx-auto animate-float bg-neutral-300/50 dark:bg-neutral-800/50 px-4 py-2 rounded-full text-sm flex items-center'><span className='w-[6px] h-[6px] inline-block rounded-full bg-teal-600 dark:bg-teal-300 mr-2 animate-pulse'></span>Available for work</p>   */}
+            <p className='flex items-center w-fit md:mx-0 mx-auto bg-neutral-300/50 dark:bg-neutral-800/40 px-4 py-2 rounded-full text-sm animate-float'><span className='w-[6px] h-[6px] rounded-full bg-teal-600 dark:bg-teal-300 mr-2 animate-pulse'></span>Based from Malaysia<img className='w-4 ml-2 inline' src={malaysia} alt="malaysia flag" /></p>  
+            <h1 className='lg:text-6xl md:text-5xl sm:text-4xl text-3xl font-semibold'> Hi, I'm <span className='gradient-text font-bold'>Wan Firdaus</span> <span className=" animate-wave">👋 </span></h1>
+            <h2 className="md:text-3xl sm:text-2xl text-xl font-light">A <span className=' font-semibold'>frontend developer</span> that love to learn and build things.
             </h2>
           </div>
           
