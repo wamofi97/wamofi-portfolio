e44ab3b7e7f9ade463e2be7a390d8bfce941b6f9
Wan Mohd Firdaus <wamofi97@gmail.com>
2024-11-17 19:55:04 +0800

    update ui

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -14,88 +14,100 @@ const skills = [
   {
     title: "JavaScript",
     source: javascript,
-    desc: "Language"
+    desc: "Language",
   },
   {
     title: "Vite",
     source: vite,
-    desc: "Frontend build tool"
+    desc: "Frontend build tool",
   },
   {
     title: "React",
     source: react,
-    desc: "Frontend framework"
+    desc: "Frontend framework",
   },
   {
     title: "TypeScript",
     source: typescript,
-    desc: "Language"
+    desc: "Language",
   },
   {
     title: "Tailwind",
     source: tailwind,
-    desc: "CSS framework"
-  },   
+    desc: "CSS framework",
+  },
   {
     title: "Next.js",
     source: nextjs,
-    desc: "Fullstack framework"
+    desc: "Fullstack framework",
   },
   {
     title: "Node.js",
     source: nodejs,
-    desc: "Backend framework"
+    desc: "Backend framework",
   },
   {
     title: "Express",
     source: express,
-    desc: "Backend framework"
+    desc: "Backend framework",
   },
   {
     title: "PostgreSQL",
     source: postgres,
-    desc: "Database"
+    desc: "Database",
   },
   {
     title: "Github",
     source: github,
-    desc: "Version control"
+    desc: "Version control",
   },
   {
     title: "Figma",
     source: figma,
-    desc: "Design tool"
+    desc: "Design tool",
   },
-]
+];
 
 export function Skills() {
   return (
-    <section  id="skills" className="h-full flex flex-col justify-center py-20 px-4 relative">
-      
-      <div className=" mx-auto px-4">
-        <div className="text-center mb-16">
-          <h2 className="md:text-6xl sm:text-5xl text-[2.5rem]  font-heading inline-flex items-center gap-2">
+    <section
+      id="skills"
+      className="relative flex h-full flex-col justify-center px-4 py-20"
+    >
+      <div className="mx-auto px-4">
+        <div className="mb-16 text-center">
+          <h2 className="inline-flex items-center gap-2 font-heading text-[2.5rem] sm:text-5xl md:text-6xl">
             SKILLS
           </h2>
-          <h3 className="text-teal-700 text-lg font-medium">My go-to technologies.</h3>
-          
+          <h3 className="text-lg font-medium text-teal-700">
+            My go-to technologies.
+          </h3>
         </div>
       </div>
-      <div className="flex flex-wrap justify-center gap-3 sm:gap-4 mx-auto">
+      <div className="mx-auto flex flex-wrap justify-center gap-3 sm:gap-4">
         {skills.map((skill, index) => (
-            <div key={index} className="group bg-neutral-300/60 dark:bg-neutral-900/60 hover:bg-teal-600/20 dark:hover:bg-teal-900/20 rounded-2xl p-5">
-                <div className=" flex flex-col sm:flex-row items-center gap-2 sm:gap-4 ">
-                    <img src={skill.source} alt={skill.title} className={`${skill.source === github ||  skill.source === nextjs || skill.source === express  ? "dark:invert" : ""}  h-8 object-cover group-hover:scale-110 group-hover:rotate-12 transition-transform duration-500`} />
-                    <div>
-                        <h3 className="text-lg font-body">{skill.title}</h3>
-                        <p className="text-neutral-500 text-sm md:block hidden">{skill.desc}</p>
-                    </div>
-                </div>
+          <div
+            key={index}
+            className="group rounded-2xl bg-neutral-300/60 p-5 hover:bg-teal-600/20 dark:bg-neutral-900/60 dark:hover:bg-teal-900/20"
+          >
+            <div className="flex flex-col items-center gap-2 sm:flex-row sm:gap-4">
+              <img
+                src={skill.source}
+                alt={skill.title}
+                className={`${skill.source === github || skill.source === nextjs || skill.source === express ? "dark:invert" : ""} h-8 object-cover transition-transform duration-500 group-hover:rotate-12 group-hover:scale-110`}
+              />
+              <div>
+                <h3 className="font-body text-lg">{skill.title}</h3>
+                <p className="hidden text-sm text-neutral-500 md:block">
+                  {skill.desc}
+                </p>
+              </div>
             </div>
+          </div>
         ))}
       </div>
-      <div className="absolute top-10 sm:right-0 -right-20 sm:w-1/5 w-1/2 h-1/3 bg-teal-400 rounded-2xl blur-2xl opacity-20"></div>
-      <div className="absolute -bottom-32 sm:left-10 -left-20 sm:w-1/5 w-1/2 h-1/3 bg-teal-400 rounded-3xl blur-3xl opacity-20"></div>
+      <div className="absolute -right-20 top-10 h-1/3 w-1/2 rounded-2xl bg-teal-400 opacity-20 blur-2xl sm:right-0 sm:w-1/5"></div>
+      <div className="absolute -bottom-32 -left-20 h-1/3 w-1/2 rounded-3xl bg-teal-400 opacity-20 blur-3xl sm:left-10 sm:w-1/5"></div>
     </section>
   );
 }
