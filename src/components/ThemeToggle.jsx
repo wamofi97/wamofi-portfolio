05dffbca64f2b0ba2f27edc4f5f6264f89831a83
Wan Mohd Firdaus <wamofi97@gmail.com>
2024-11-17 19:52:12 +0800

    add usesound for themetoggle

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,42 +1,45 @@
 // ThemeToggle.js
-import { useEffect, useState } from 'react';
-import { Sun, Moon } from 'lucide-react';
+import { useEffect, useState } from "react";
+import { Sun, Moon } from "lucide-react";
+
+import useSound from "use-sound";
+
+import switchon from "/switchon.mp3";
+import switchoff from "/switchoff.mp3";
 
 const ThemeToggle = () => {
   const [darkMode, setDarkMode] = useState(false);
+  const [lightsOn, setLightsOn] = useState(false);
+  const [play] = useSound(lightsOn ? switchon : switchoff, { volume: 0.4 });
 
   useEffect(() => {
-    const savedTheme = localStorage.getItem('theme');
-    if (savedTheme === 'dark' || savedTheme === null) {
+    const savedTheme = localStorage.getItem("theme");
+    if (savedTheme === "dark" || savedTheme === null) {
       setDarkMode(true);
-      document.documentElement.classList.add('dark');
-    } else if (savedTheme === 'light') {
+      document.documentElement.classList.add("dark");
+    } else if (savedTheme === "light") {
       setDarkMode(false);
-      document.documentElement.classList.remove('dark');
+      document.documentElement.classList.remove("dark");
     }
   }, []);
 
   const toggleDarkMode = () => {
     setDarkMode(!darkMode);
-    document.documentElement.classList.toggle('dark', !darkMode);
-    localStorage.setItem('theme', darkMode ? 'light' : 'dark');
+    setLightsOn(!lightsOn);
+    play();
+    document.documentElement.classList.toggle("dark", !darkMode);
+    localStorage.setItem("theme", darkMode ? "light" : "dark");
   };
 
   return (
-    <div className="fixed max-w-5xl z-30 top-0 w-full"> 
-      <button onClick={toggleDarkMode} 
-      className={`absolute md:top-[23px] top-[11px] md:right-4 right-[72px] flex items-center justify-center bg-neutral-200 rounded-2xl w-10 h-10 transition-colors bg-transparent
-      ${darkMode ? 'bg-transparent md:bg-neutral-800 md:hover:bg-neutral-700' : 'bg-transparent md:bg-neutral-300/80 md:hover:bg-neutral-400'}`}>
-      {darkMode ? (
-        <Moon className="w-7 h-7" />
-      ) : (
-        <Sun className="w-7 h-7" />
-      )}
-
+    <div className="fixed top-0 z-30 w-full max-w-5xl">
+      <button
+        onClick={toggleDarkMode}
+        className={`absolute right-[72px] top-[11px] flex h-10 w-10 items-center justify-center rounded-2xl bg-neutral-200 bg-transparent transition-colors md:right-4 md:top-[23px] ${darkMode ? "bg-transparent md:bg-neutral-800 md:hover:bg-neutral-700" : "bg-transparent md:bg-neutral-300/80 md:hover:bg-neutral-400"}`}
+      >
+        {darkMode ? <Moon className="h-7 w-7" /> : <Sun className="h-7 w-7" />}
       </button>
-      
     </div>
-  
   );
 };
 
